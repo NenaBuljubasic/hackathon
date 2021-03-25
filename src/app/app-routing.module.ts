e3de527a7d8a474c45bbcf8e2d69c33bb70e197e
sime1997 <sime.gaurina@gmail.com>
2021-03-25 20:51:35 +0100

    nanovo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdministracijaComponent } from './administracija/administracija.component';
-import { HomeComponent } from './home/home.component';
+
 import { RezervacijeComponent } from './rezervacije/rezervacije.component';
 
 //const routes: Routes = [];
 const routes: Routes = [
-  { path:'home' , component: HomeComponent  },
+  
   { path:'rezervacije', component: RezervacijeComponent },
   { path:'administracija'   , component: AdministracijaComponent  }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RezervacijeComponent } from './rezervacije/rezervacije.component';
-import { HomeComponent } from './home/home.component';
-import { StatusComponent } from './status/status.component';
+
+
 import { AdministracijaComponent } from './administracija/administracija.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RezervacijeComponent,
-    HomeComponent,
-    StatusComponent,
     AdministracijaComponent
   ],
   imports: [
