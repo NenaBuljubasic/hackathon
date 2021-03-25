3bd8a4db73eafdde7b24be2af9b7a5ab6640e9ab
NenaBuljuabsic <nena.buljubasic@gmail.com>
2021-03-25 11:58:41 +0100

    druga promjena

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdiministracijaComponent } from './adiministracija/adiministracija.component';
+import { HomeComponent } from './home/home.component';
+import { RezervacijeComponent } from './rezervacije/rezervacije.component';
 
-const routes: Routes = [];
+//const routes: Routes = [];
+const routes: Routes = [
+  { path:'home' , component: HomeComponent  },
+  { path:'rezervacije', component: RezervacijeComponent },
+  { path:'**'   , component: AdiministracijaComponent  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
